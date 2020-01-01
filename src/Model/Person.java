bc86543b8e69966b89b08d45ac32d4c8340a3acd
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-01 13:15:39 +0100

    Add the atributes and getters.

---
+++ src/Model/Person.java
@@ -0,0 +1,24 @@
+package Model;
+
+import java.util.Date;
+
+public class Person {
+    private final String name;
+    private final Date birthday;
+
+    public Person(String name, Date birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+    
+    
+    
+}
