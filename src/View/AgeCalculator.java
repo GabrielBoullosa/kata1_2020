481e71d93849185a1fd63c75a62cdb6335381807
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-01 13:29:01 +0100

    Add to the AgeCalculator the methods to calculate the age.

--- src/View/AgeCalculator.java
+++ src/View/AgeCalculator.java
@@ -1,12 +1,24 @@
 package View;
 
+import java.util.Date;
+
 public class AgeCalculator {
     private final static int ms_per_s = 1000;
     private final static int s_per_m = 60;
     private final static int m_per_h = 60;
     private final static int h_per_d = 24;
     private final static double d_per_y = 365.5;
+    private final static double ms_per_y = ms_per_s *
+            s_per_m * m_per_h * h_per_d;
     
-    
+    public int getAge(Date birthday){
+        Date date = new Date();
+        long ms = date.getTime() - birthday.getTime();
+        return toYears(ms);
+    }
+
+    private int toYears(long ms) {
+        return (int) (ms/ms_per_y);
+    }
     
 }
