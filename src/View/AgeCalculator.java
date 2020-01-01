56066be8bf8477c4545a1bc1293970968bd7dae7
gabriel_hijo <gabriel_hijo@gabriel_hijo-PC.home>
2020-01-01 13:22:29 +0100

    Add a view class that do calculate the age.

---
+++ src/View/AgeCalculator.java
@@ -0,0 +1,12 @@
+package View;
+
+public class AgeCalculator {
+    private final static int ms_per_s = 1000;
+    private final static int s_per_m = 60;
+    private final static int m_per_h = 60;
+    private final static int h_per_d = 24;
+    private final static double d_per_y = 365.5;
+    
+    
+    
+}
